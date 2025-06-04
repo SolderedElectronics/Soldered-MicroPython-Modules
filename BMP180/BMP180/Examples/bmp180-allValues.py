042f31771a3c65f5108f0532d776a1659718458b
JosipKuci <JosipKuci@users.noreply.github.com>
2025-06-04 14:29:33 +0000

    Apply Ruff formatting (MicroPython-safe)

--- BMP180/BMP180/Examples/bmp180-allValues.py
+++ BMP180/BMP180/Examples/bmp180-allValues.py
@@ -3,26 +3,26 @@
 # BRIEF: An example for the BME280 sensor that reads temperature and pressure,
 # WORKS WITH: Pressure & temperature sensor BMP180 breakout: www.solde.red/333060
 # LAST UPDATED: 2025-06-04
-from machine import I2C, Pin #Import for manual pin configuration
-from bmp180 import BMP180 #Import the module
-import time #Used for the sleep() function
+from machine import I2C, Pin  # Import for manual pin configuration
+from bmp180 import BMP180  # Import the module
+import time  # Used for the sleep() function
 
-#If you're not using the Qwiic connector, manually configure your I2C connection
-#i2c = I2C(1, scl=Pin(22), sda=Pin(21))
-#bmp180 = BMP180(i2c)
+# If you're not using the Qwiic connector, manually configure your I2C connection
+# i2c = I2C(1, scl=Pin(22), sda=Pin(21))
+# bmp180 = BMP180(i2c)
 
-#Create an instance of the sensor object
+# Create an instance of the sensor object
 bmp180 = BMP180()
 
-#Infinite loop
+# Infinite loop
 while 1:
-    #Get tempreature and pressure readings from sensor
+    # Get tempreature and pressure readings from sensor
     temperature = bmp180.read_temperature()
     pressure = bmp180.read_pressure()
-    
-    #Print the measured values to the second decimal point
+
+    # Print the measured values to the second decimal point
     print("Temperature: {:.2f} C".format(temperature))
     print("Pressure: {} hPa\n".format(pressure))
-    
-    #Pause for 5 seconds
+
+    # Pause for 5 seconds
     time.sleep(5.0)
--- BMP180/BMP180/bmp180.py
+++ BMP180/BMP180/bmp180.py
@@ -19,6 +19,7 @@ REG_DATA = 0xF6
 CMD_READ_TEMP = 0x2E
 CMD_READ_PRESSURE = 0x34
 
+
 class BMP180:
     def __init__(self, i2c=None, address: int = BMP180_I2C_ADDR, oss: int = 0):
         """
@@ -46,19 +47,19 @@ class BMP180:
         Reads calibration coefficients from the BMP180's EEPROM.
         """
         raw = self.i2c.readfrom_mem(self.address, REG_CALIB, 22)
-        vals = struct.unpack('>hhhHHHhhhhh', raw)
+        vals = struct.unpack(">hhhHHHhhhhh", raw)
 
-        self.cal['AC1'] = vals[0]
-        self.cal['AC2'] = vals[1]
-        self.cal['AC3'] = vals[2]
-        self.cal['AC4'] = vals[3]
-        self.cal['AC5'] = vals[4]
-        self.cal['AC6'] = vals[5]
-        self.cal['B1'] = vals[6]
-        self.cal['B2'] = vals[7]
-        self.cal['MB'] = vals[8]
-        self.cal['MC'] = vals[9]
-        self.cal['MD'] = vals[10]
+        self.cal["AC1"] = vals[0]
+        self.cal["AC2"] = vals[1]
+        self.cal["AC3"] = vals[2]
+        self.cal["AC4"] = vals[3]
+        self.cal["AC5"] = vals[4]
+        self.cal["AC6"] = vals[5]
+        self.cal["B1"] = vals[6]
+        self.cal["B2"] = vals[7]
+        self.cal["MB"] = vals[8]
+        self.cal["MC"] = vals[9]
+        self.cal["MD"] = vals[10]
 
     def _read_raw_temp(self):
         """
@@ -67,16 +68,18 @@ class BMP180:
         self.i2c.writeto_mem(self.address, REG_CONTROL, bytes([CMD_READ_TEMP]))
         time.sleep_ms(5)
         raw = self.i2c.readfrom_mem(self.address, REG_DATA, 2)
-        return struct.unpack('>H', raw)[0]
+        return struct.unpack(">H", raw)[0]
 
     def _read_raw_pressure(self):
         """
         Starts pressure measurement and returns raw pressure.
         """
-        self.i2c.writeto_mem(self.address, REG_CONTROL, bytes([CMD_READ_PRESSURE + (self.oss << 6)]))
+        self.i2c.writeto_mem(
+            self.address, REG_CONTROL, bytes([CMD_READ_PRESSURE + (self.oss << 6)])
+        )
         time.sleep_ms(2 + (3 << self.oss))
         raw = self.i2c.readfrom_mem(self.address, REG_DATA, 3)
-        msb, lsb, xlsb = struct.unpack('BBB', raw)
+        msb, lsb, xlsb = struct.unpack("BBB", raw)
         return ((msb << 16) + (lsb << 8) + xlsb) >> (8 - self.oss)
 
     def read_temperature(self) -> float:
@@ -84,8 +87,8 @@ class BMP180:
         Reads and returns the true temperature in Celsius.
         """
         UT = self._read_raw_temp()
-        X1 = ((UT - self.cal['AC6']) * self.cal['AC5']) >> 15
-        X2 = (self.cal['MC'] << 11) // (X1 + self.cal['MD'])
+        X1 = ((UT - self.cal["AC6"]) * self.cal["AC5"]) >> 15
+        X2 = (self.cal["MC"] << 11) // (X1 + self.cal["MD"])
         self._B5 = X1 + X2
         T = (self._B5 + 8) >> 4
         return T / 10.0
@@ -98,15 +101,15 @@ class BMP180:
         UP = self._read_raw_pressure()
 
         B6 = self._B5 - 4000
-        X1 = (self.cal['B2'] * (B6 * B6 >> 12)) >> 11
-        X2 = (self.cal['AC2'] * B6) >> 11
+        X1 = (self.cal["B2"] * (B6 * B6 >> 12)) >> 11
+        X2 = (self.cal["AC2"] * B6) >> 11
         X3 = X1 + X2
-        B3 = (((self.cal['AC1'] * 4 + X3) << self.oss) + 2) >> 2
+        B3 = (((self.cal["AC1"] * 4 + X3) << self.oss) + 2) >> 2
 
-        X1 = (self.cal['AC3'] * B6) >> 13
-        X2 = (self.cal['B1'] * (B6 * B6 >> 12)) >> 16
+        X1 = (self.cal["AC3"] * B6) >> 13
+        X2 = (self.cal["B1"] * (B6 * B6 >> 12)) >> 16
         X3 = ((X1 + X2) + 2) >> 2
-        B4 = (self.cal['AC4'] * (X3 + 32768)) >> 15
+        B4 = (self.cal["AC4"] * (X3 + 32768)) >> 15
         B7 = (UP - B3) * (50000 >> self.oss)
 
         if B7 < 0x80000000:
@@ -118,4 +121,4 @@ class BMP180:
         X1 = (X1 * 3038) >> 16
         X2 = (-7357 * P) >> 16
         P = P + ((X1 + X2 + 3791) >> 4)
-        return P/100
+        return P / 100
