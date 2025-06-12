8ebee7a00948c85cd366419edc520112bc4f8204
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-06-12 14:22:48 +0200

    Add Ultrasonic sensor module

---
+++ UltrasonicSensor/UltrasonicSensor/Examples/UltrasonicSensor-readDistanceNative.py
@@ -0,0 +1,31 @@
+# FILE: UltrasonicSensor-readDistanceNative.py 
+# AUTHOR: Josip Šimun Kuči @ Soldered
+# BRIEF:  An example showing how to read the distance of the Native Ultrasonic sensor
+# WORKS WITH: Ultrasonic module HC-SR04: www.solde.red/101202
+# LAST UPDATED: 2025-06-12 
+
+from machine import I2C, Pin
+from UltrasonicSensor import UltrasonicSensor
+import time
+
+# Create sensor instance
+sensor = UltrasonicSensor()
+
+while True:
+    sensor.takeMeasure()
+    time.sleep(0.1)  # Small delay for measurement
+    
+    #The sensor sends a pulse through the trigger pin, which is deflected
+    #By a surface in front of it and picked up by the echo pin, we then use the
+    #time it took the impulse to reflect to calculate the distance
+    distance = sensor.getDistance()
+    
+    #How long did it take for the pulse to reflect
+    duration = sensor.getDuration()
+    
+    #Print out the distance and duration to the user
+    print(f"Distance: {distance:.2f} cm")
+    print(f"Time it took: {duration:.2f} us\n")
+    
+    
+    time.sleep(0.25)  # Delay between readings
