f56f2d0867a4e029113fec71168e1f9702243712
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-06-12 14:32:29 +0200

    Fix native example for ultrasonic sensor

--- UltrasonicSensor/UltrasonicSensor/Examples/UltrasonicSensor-readDistanceNative.py
+++ UltrasonicSensor/UltrasonicSensor/Examples/UltrasonicSensor-readDistanceNative.py
@@ -9,23 +9,22 @@ from UltrasonicSensor import UltrasonicSensor
 import time
 
 # Create sensor instance
-sensor = UltrasonicSensor()
+sensor = UltrasonicSensor(trig_pin=2,echo_pin=35)
 
 while True:
-    sensor.takeMeasure()
     time.sleep(0.1)  # Small delay for measurement
-    
-    #The sensor sends a pulse through the trigger pin, which is deflected
-    #By a surface in front of it and picked up by the echo pin, we then use the
-    #time it took the impulse to reflect to calculate the distance
+
+    # The sensor sends a pulse through the trigger pin, which is deflected
+    # By a surface in front of it and picked up by the echo pin, we then use the
+    # time it took the impulse to reflect to calculate the distance
     distance = sensor.getDistance()
-    
-    #How long did it take for the pulse to reflect
+
+    time.sleep(0.1)  # Small delay for measurement
+    # How long did it take for the pulse to reflect
     duration = sensor.getDuration()
-    
-    #Print out the distance and duration to the user
+
+    # Print out the distance and duration to the user
     print(f"Distance: {distance:.2f} cm")
     print(f"Time it took: {duration:.2f} us\n")
-    
-    
+
     time.sleep(0.25)  # Delay between readings
